88424650e16aaa358736e9869a6110726920a04b
s.sveshnikov <s.sveshnikov@tinkoff.ru>
2019-03-29 17:05:59 +0300

    single IMain instance wrapped with synchromized

--- core/src/main/scala/ru/tinkoff/codefest/executor/Interpretator.scala
+++ core/src/main/scala/ru/tinkoff/codefest/executor/Interpretator.scala
@@ -1,25 +1,44 @@
 package ru.tinkoff.codefest.executor
 
 import java.io.{ByteArrayOutputStream, FileInputStream, PrintWriter}
-
 import scala.tools.nsc.Settings
 import scala.tools.nsc.interpreter.{IMain, IR}
+import scala.util.Try
+import scala.util.control.NonFatal
+
 import cats.data.NonEmptyList
 import cats.effect._
 import cats.syntax.option._
 import io.circe.generic.JsonCodec
 import io.circe.{Decoder, Encoder}
-import ru.tinkoff.codefest.executor.Interpretator.{IRState, Result}
 
-import scala.util.Try
+import ru.tinkoff.codefest.executor.Interpretator.{IRState, Result}
 
 class Interpretator[F[_]: Sync] {
   import Interpretator.EOL
 
   private val F: Sync[F] = implicitly[Sync[F]]
 
+  val settings = new Settings
+  val args = List(
+    "-deprecation",
+    "-feature",
+    "-unchecked",
+    "-language:postfixOps",
+    "-language:higherKinds",
+    "-language:implicitConversions",
+    "-Ypartial-unification",
+    "-Xplugin:kind-projector"
+  )
+  settings.processArgumentString(args.mkString(" "))
+  settings.usejavacp.value = true
+  val in = new FileInputStream("/dev/null")
+  val out = new ByteArrayOutputStream
+  val writer = new PrintWriter(out)
+  val intp = new IMain(settings, writer)
+
   def interpret(code: NonEmptyList[String]): F[Result] = {
-    val settings = new Settings
+    /*val settings = new Settings
     val args = List(
       "-deprecation",
       "-feature",
@@ -31,7 +50,7 @@ class Interpretator[F[_]: Sync] {
       "-Xplugin:kind-projector"
     )
     settings.processArgumentString(args.mkString(" "))
-    settings.usejavacp.value = true
+    settings.usejavacp.value = true*/
     //settings.usemanifestcp.value = true использовать если cp, определен в манифесте, например для LauncherJarPlugin
 
     val foldFunction: IMain => (IRState, String) => IRState = intp => {
@@ -48,25 +67,33 @@ class Interpretator[F[_]: Sync] {
           case IR.Error      => IRState(IR.Error, state.compiled)
         } getOrElse IRState(IR.Error, state.compiled)
     }
-    val res = for {
+    /* val res = for {
       out <- Resource.fromAutoCloseable(F.delay(new ByteArrayOutputStream))
       in <- Resource.fromAutoCloseable(F.delay(new FileInputStream("/dev/null")))
       writer <- Resource.fromAutoCloseable(F.delay(new PrintWriter(out)))
     } yield {
-      val intp = new IMain(settings, writer)
-      val state = Console.withIn(in) {
-        Console.withOut(out) {
-          val current = code.tail.flatMap(_.split(EOL)).foldLeft(IRState())(foldFunction(intp))
-          out.reset()
-          code.head.split(EOL).foldLeft(current)(foldFunction(intp))
+      //val intp = new IMain(settings, writer)
+     */
+    this.synchronized {
+      try {
+        val state = Console.withIn(in) {
+          Console.withOut(out) {
+            val current = code.tail.flatMap(_.split(EOL)).foldLeft(IRState())(foldFunction(intp))
+            out.reset()
+            code.head.split(EOL).foldLeft(current)(foldFunction(intp))
+          }
         }
+        out.flush()
+        val compiled = List(state.compiled, state.incompleteBuffer).flatten.mkString(EOL)
+        F.pure(Result(state.status, out.toString, compiled.some.filter(_.trim.nonEmpty)))
+      } catch {
+        case NonFatal(e) => F.raiseError(e)
+      } finally {
+        intp.reset()
+        out.reset()
       }
-      intp.close()
-      out.flush()
-      val compiled = List(state.compiled, state.incompleteBuffer).flatten.mkString(EOL)
-      Result(state.status, out.toString, compiled.some.filter(_.trim.nonEmpty))
     }
-    res.use(F.pure)
+    // res.use(F.pure)
   }
 }
 
